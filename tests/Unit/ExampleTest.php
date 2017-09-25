eba091f5da43cd2df37d138eb2a3383b725d471d
AdarshGupta <adarsh035@gmail.com>
2017-09-25 23:22:24 +0530

    PhpUnit Testing basic

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,8 +5,16 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+// New ones
+use App\Post;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
 class ExampleTest extends TestCase
 {
+	use DatabaseTransactions; // rolls back the values added in the database thus avoids accumulating records when we re-run the unit test
+
     /**
      * A basic test example.
      *
@@ -14,6 +22,29 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // Given - I have two records in the database that are posts, and each one is posted a month apart.
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+        	'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        //When - I fetch the archives
+        $posts = Post::archives();
+
+        //Then - the response should be in proper format
+        //$this->assertCount(2, $posts);
+        // To be more specific - 
+        $this->assertEquals([
+        	[
+        		"year" => $first->created_at->format('Y'),
+        		"month" => $first->created_at->format('F'),
+        		"published" => 1
+        	],
+        	[
+        		"year" => $second->created_at->format('Y'),
+        		"month" => $second->created_at->format('F'),
+        		"published" => 1
+        	]
+        ], $posts);
     }
 }
